2a629f53f142d6f67d3e9428bd8094007080cc4c
James Cocker <git@pswd.biz>
2019-09-10 16:56:55 +0100

    Added ability to specify articles per page via CMS

--- src/Model/NewsHolder.php
+++ src/Model/NewsHolder.php
@@ -3,23 +3,29 @@
 namespace PurpleSpider\BasicNews;
 
 use Page;
-use PurpleSpider\BasicNews\NewsArticle;
-use SilverStripe\Control\RSS\RSSFeed;
-use SilverStripe\Control\Director;
-use SilverStripe\View\Requirements;
-use SilverStripe\SiteConfig\SiteConfig;
-use SilverStripe\ORM\PaginatedList;
 use PageController;
 use SilverStripe\Forms\Tab;
+use SilverStripe\Control\Director;
 use SilverStripe\Forms\FieldGroup;
+use SilverStripe\ORM\PaginatedList;
+use SilverStripe\View\Requirements;
+use SilverStripe\Forms\NumericField;
+use SilverStripe\Control\RSS\RSSFeed;
 use SilverStripe\Forms\CheckboxField;
+use PurpleSpider\BasicNews\NewsArticle;
+use SilverStripe\SiteConfig\SiteConfig;
 
 class NewsHolder extends Page
 {
-  
+
     private static $db = array(
-      "DisplayFullPosts" => "Boolean"
+        "DisplayFullPosts" => "Boolean",
+        "PostsPerPage" => "Int",
     );
+
+    private static $defaults = [
+        "PostsPerPage" => 10,
+    ];
 
     public function getCMSFields()
     {
@@ -29,15 +35,17 @@ class NewsHolder extends Page
         });
 
         $fields = parent::getCMSFields();
-        
-        $fields->dataFieldByName("Content")->setDescription("This content appears at the top of the main ".$this->Title." page, above the list of news articles.");
+
+        $fields->dataFieldByName("Content")->setDescription("This content appears at the top of the main " . $this->Title . " page, above the list of news articles.");
 
         return $fields;
     }
-    
-    function getSettingsFields() {
+
+    function getSettingsFields()
+    {
         $fields = parent::getSettingsFields();
-        $fields->addFieldToTab("Root.Settings", FieldGroup::create(CheckboxField::create('DisplayFullPosts', 'Display full articles?'))->setTitle("News Articles")); 
+        $fields->addFieldToTab("Root.Settings", FieldGroup::create(CheckboxField::create('DisplayFullPosts', 'Display full articles?'))->setTitle("News articles"));
+        $fields->addFieldToTab("Root.Settings", NumericField::create('PostsPerPage', 'Articles per page'));
         return $fields;
     }
 
@@ -45,25 +53,25 @@ class NewsHolder extends Page
     {
         return "News Articles";
     }
-    
+
     public function getLumberjackPagesForGridfield($excluded = array())
-  	{
-  			return NewsArticle::get()->filter(array(
-  					'ParentID' => $this->ID,
-  					'ClassName' => $excluded,
-  			));
-  	}
+    {
+        return NewsArticle::get()->filter(array(
+            'ParentID' => $this->ID,
+            'ClassName' => $excluded,
+        ));
+    }
 
     // Only allows certain children to be created
     private static $allowed_children = array(NewsArticle::class);
     private static $icon_class = 'font-icon-news';
     private static $table_name = "NewsHolder";
     private static $description = 'Displays multiple News Articles';
-    
-    
+
+
     public function stageChildren($showAll = false)
     {
-        return $this->__call('stageChildren', array($showAll))->sort(array('MenuTitle'=>'DESC', "Created"=>'DESC'));
+        return $this->__call('stageChildren', array($showAll))->sort(array('MenuTitle' => 'DESC', "Created" => 'DESC'));
     }
 
     public function init()
@@ -80,7 +88,7 @@ class NewsHolder_Controller extends PageController
     private static $allowed_actions = array(
         "rss"
     );
-    
+
     public function rss()
     {
         $config = SiteConfig::current_site_config();
@@ -89,17 +97,21 @@ class NewsHolder_Controller extends PageController
             $list = NewsArticle::get(), // an SS_List containing your feed items
             $link = $this->Link("rss"), // a HTTP link to this feed
             $title = $config->Title . " News", // title for this feed, displayed in RSS readers
-            $description = "All the latest news from ". $config->Title . "." // description
+            $description = "All the latest news from " . $config->Title . "." // description
         );
         // Outputs the RSS feed to the user.
         return $rss->outputToBrowser();
     }
-    
+
     // Provides Paginated List of NewsArticles
     public function PaginatedPages()
     {
         $list = new PaginatedList(NewsArticle::get()->filter("ParentID", $this->ID), $this->request);
-        $list->setPageLength(10);
+        if ($this->PostsPerPage) {
+            $list->setPageLength($this->PostsPerPage);
+        } else {
+            $list->setPageLength(10);
+        }
         return $list;
     }
 }
